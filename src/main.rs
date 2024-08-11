6b899dbb192165830d0abe8534f85dd817287925
Phillip Schichtel <phillip@schich.tel>
2024-08-11 23:12:52 +0200

    format

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
-use std::{env, fmt::Display, net::SocketAddr, process::ExitCode, sync::{mpsc::{self, Sender}, Arc}, thread::{self, JoinHandle}, time::Duration};
+use std::{
+    env,
+    fmt::Display,
+    net::SocketAddr,
+    process::ExitCode,
+    sync::{
+        mpsc::{self, Sender},
+        Arc,
+    },
+    thread::{self, JoinHandle},
+    time::Duration,
+};
 
 use redis::{cmd, Client, Cmd, Connection, ControlFlow, PubSubCommands, RedisError};
 
@@ -27,14 +38,20 @@ impl Display for Error {
 
 type RedisAddr = (String, u16);
 
-fn get_master_from_sentinel(connection: &mut Connection, master_name: &str) -> Result<RedisAddr, Error> {
-    let response = match get_master_from_sentinel_cmd(master_name).query::<Vec<String>>(connection) {
+fn get_master_from_sentinel(
+    connection: &mut Connection,
+    master_name: &str,
+) -> Result<RedisAddr, Error> {
+    let response = match get_master_from_sentinel_cmd(master_name).query::<Vec<String>>(connection)
+    {
         Ok(response) => response,
         Err(redis_err) => return Err(Error::RedisErr(redis_err)),
     };
 
     if response.len() != 2 {
-        return Err(Error::InvalidResponse("Response did not have exactly 2 elements!".to_owned()))
+        return Err(Error::InvalidResponse(
+            "Response did not have exactly 2 elements!".to_owned(),
+        ));
     }
 
     let host: String = response[0].to_owned();
@@ -44,69 +61,80 @@ fn get_master_from_sentinel(connection: &mut Connection, master_name: &str) -> R
     };
 
     return Ok((host, port));
-
 }
 
-fn listen_for_master_switches(client: Arc<Client>, sender: Sender<RedisAddr>, master_name: &str) -> JoinHandle<()> {
+fn listen_for_master_switches(
+    client: Arc<Client>,
+    sender: Sender<RedisAddr>,
+    master_name: &str,
+) -> JoinHandle<()> {
     let master_name = master_name.to_string();
-    return thread::spawn(move || {
-        loop {
-            let mut connection = match client.get_connection() {
-                Ok(c) => c,
-                Err(err) => {
-                    eprintln!("Failed to connect: {}", err);
-                    continue;
-                },
-            };
-            let topic = "+switch-master";
-            let subscribe_result = connection.subscribe::<_, _, ()>(topic, |msg| {
-                let value: String = msg.get_payload().unwrap();
-                let segments: Vec<&str> = value.as_str().split_ascii_whitespace().into_iter().collect();
-                if segments.len() < 5 {
-                    eprintln!("Received invalid switch-master event: {:?}", segments);
-                    return ControlFlow::Continue;
-                }
-                let affected_master = segments[0];
-                if master_name.as_str() != affected_master {
-                    println!("Master changed for {}, we are not interested in that...", affected_master);
-                    return ControlFlow::Continue;
-                }
-                let host = segments[3].to_owned();
-                let port: u16 = segments[4].parse().unwrap();
-                sender.send((host, port)).unwrap();
-                ControlFlow::Continue
-            });
-
-            if let Err(err) = subscribe_result {
-                eprintln!("Failed to subscribe to topic {}: {}", topic, err);
+    return thread::spawn(move || loop {
+        let mut connection = match client.get_connection() {
+            Ok(c) => c,
+            Err(err) => {
+                eprintln!("Failed to connect: {}", err);
                 continue;
             }
+        };
+        let topic = "+switch-master";
+        let subscribe_result = connection.subscribe::<_, _, ()>(topic, |msg| {
+            let value: String = msg.get_payload().unwrap();
+            let segments: Vec<&str> = value
+                .as_str()
+                .split_ascii_whitespace()
+                .into_iter()
+                .collect();
+            if segments.len() < 5 {
+                eprintln!("Received invalid switch-master event: {:?}", segments);
+                return ControlFlow::Continue;
+            }
+            let affected_master = segments[0];
+            if master_name.as_str() != affected_master {
+                println!(
+                    "Master changed for {}, we are not interested in that...",
+                    affected_master
+                );
+                return ControlFlow::Continue;
+            }
+            let host = segments[3].to_owned();
+            let port: u16 = segments[4].parse().unwrap();
+            sender.send((host, port)).unwrap();
+            ControlFlow::Continue
+        });
+
+        if let Err(err) = subscribe_result {
+            eprintln!("Failed to subscribe to topic {}: {}", topic, err);
+            continue;
         }
     });
 }
 
-fn poll_master_address(client: Arc<Client>, sender: Sender<RedisAddr>, master_name: &str, poll_interval: &Duration) -> JoinHandle<()> {
+fn poll_master_address(
+    client: Arc<Client>,
+    sender: Sender<RedisAddr>,
+    master_name: &str,
+    poll_interval: &Duration,
+) -> JoinHandle<()> {
     let master_name = master_name.to_string();
     let poll_interval = *poll_interval;
-    return thread::spawn(move || {
-        loop {
-            let mut connection = match client.get_connection() {
-                Ok(c) => c,
-                Err(err) => {
-                    eprintln!("Failed to connect: {}", err);
-                    continue;
-                },
-            };
-            match get_master_from_sentinel(&mut connection, master_name.as_str()) {
-                Ok(master) => {
-                    sender.send(master).unwrap();
-                },
-                Err(err) => {
-                    eprintln!("Failed to get initial master: {}", err);
-                },
-            };
-            thread::sleep(poll_interval);
-        }
+    return thread::spawn(move || loop {
+        let mut connection = match client.get_connection() {
+            Ok(c) => c,
+            Err(err) => {
+                eprintln!("Failed to connect: {}", err);
+                continue;
+            }
+        };
+        match get_master_from_sentinel(&mut connection, master_name.as_str()) {
+            Ok(master) => {
+                sender.send(master).unwrap();
+            }
+            Err(err) => {
+                eprintln!("Failed to get initial master: {}", err);
+            }
+        };
+        thread::sleep(poll_interval);
     });
 }
 
@@ -116,7 +144,7 @@ fn materialize_service(addr: &RedisAddr) {
         Err(err) => {
             eprintln!("Failed to resolve the address: {}", err);
             return;
-        },
+        }
     };
 
     for addr in socket_addrs {
@@ -128,7 +156,10 @@ fn main() -> ExitCode {
     let args: Vec<String> = env::args().collect();
     if args.len() != 4 {
         eprintln!("Wrong arguments!");
-        eprintln!("Usage: {} <sentinal host:port> <master name> <poll interval secs>", args[0]);
+        eprintln!(
+            "Usage: {} <sentinal host:port> <master name> <poll interval secs>",
+            args[0]
+        );
         return ExitCode::FAILURE;
     }
     let sentinel_addr = args[1].clone();
@@ -141,7 +172,7 @@ fn main() -> ExitCode {
         Err(err) => {
             eprintln!("Failed to get initial master: {}", err);
             return ExitCode::FAILURE;
-        },
+        }
     };
 
     println!("Master: {:?}", initial_master);
@@ -150,7 +181,12 @@ fn main() -> ExitCode {
     let (tx, rx) = mpsc::channel::<RedisAddr>();
 
     let _ = listen_for_master_switches(client.clone(), tx.clone(), master_name.as_str());
-    let _ = poll_master_address(client.clone(), tx.clone(), master_name.as_str(), &poll_interval);
+    let _ = poll_master_address(
+        client.clone(),
+        tx.clone(),
+        master_name.as_str(),
+        &poll_interval,
+    );
 
     loop {
         let addr = match rx.recv() {
@@ -158,9 +194,8 @@ fn main() -> ExitCode {
             Err(err) => {
                 eprintln!("Failed to receive: {}", err);
                 continue;
-            },
+            }
         };
-
 
         println!("Received new master: {:?}", addr);
         materialize_service(&addr);
